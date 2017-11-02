eb7255dae3414bc3bdaf86f38bd6701ae9f96bcf
Elizabeth Alias Neethi Joseph <neethi@uber.com>
2017-11-02 16:25:15 -0700

    named khaleesi

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func myLogic() (string, int, error) {
 	dataMap := map[string]string{
 		"channel":  "super-test",
 		"text":     "Danger!!",
-		"username": "Khaleesi",
+		"username": "Daenerys",
 	}
 
 	data, err := json.Marshal(dataMap)
